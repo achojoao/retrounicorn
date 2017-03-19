38d99bba2f689cba4d50010d436fffd1d84759d7
achojoao <juanvidom@gmail.com>
2017-03-19 16:04:12 -0400

    Delete retrounicorn_starter.py

--- retrounicorn_starter.py
+++
@@ -1,7 +0,0 @@
-import os
-import urllib
-try:
-    urllib.urlretrieve ("https://raw.githubusercontent.com/achojoao/retrounicorn/master/retrounicorn.py", "retrounicorn.py")
-except:
-    pass
-os.system("python retrounicorn.py")
